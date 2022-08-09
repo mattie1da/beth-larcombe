cc68c8c6a0182169a741c304ff0b0b6daa5ab5aa
Matt Wyatt <hello@mattie.world>
2022-08-09 23:38:05 +0100

    Navigation Component

--- layouts/Navigation.tsx
+++ layouts/Navigation.tsx
@@ -1,5 +1,8 @@
+import classnames from "classnames";
 import Link from "next/link";
 import { useState } from "react";
+import { IconBee } from "../components/icons";
+import styles from "../styles/layouts/Navigation.module.scss";
 
 // import a context to write open nav state to
 
@@ -11,12 +14,22 @@ export const Navigation = () => {
   };
 
   return (
-    <nav>
-      <Link href="/">bee</Link>
-      <button onClick={toggleMenu}>{menuOpen ? "close" : "open"}</button>
+    <nav
+      className={classnames(styles.wrapper, {
+        [styles.open]: menuOpen,
+      })}
+    >
+      <div className={styles.header}>
+        <Link href="/">
+          <a className={styles.icon}>
+            <IconBee />
+          </a>
+        </Link>
+        <button onClick={toggleMenu}>{menuOpen ? "close" : "open"}</button>
+      </div>
 
       {menuOpen && (
-        <ul>
+        <ul className="">
           <li>
             <Link href="/#projects">Projects</Link>
           </li>

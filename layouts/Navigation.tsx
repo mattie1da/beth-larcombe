998010ef1988a84436876d1a4e0ff43fa28b9293
Matt Wyatt <hello@mattie.world>
2022-08-09 19:40:25 +0100

    Initial Navigation WIP

--- layouts/Navigation.tsx
+++ layouts/Navigation.tsx
@@ -1,5 +1,33 @@
+import Link from "next/link";
+import { useState } from "react";
+
+// import a context to write open nav state to
+
 export const Navigation = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <nav>weee!!</nav>
-  )
-}
+    <nav>
+      <Link href="/">bee</Link>
+      <button onClick={toggleMenu}>{menuOpen ? "close" : "open"}</button>
+
+      {menuOpen && (
+        <ul>
+          <li>
+            <Link href="/#projects">Projects</Link>
+          </li>
+          <li>
+            <Link href="/#about">About</Link>
+          </li>
+          <li>
+            <Link href="/#contact">Contact</Link>
+          </li>
+        </ul>
+      )}
+    </nav>
+  );
+};

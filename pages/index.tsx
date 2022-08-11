2c22ab7ce95c1407757f3643086c64d9a7f2fec3
Matt Wyatt <hello@mattie.world>
2022-08-11 07:50:35 +0100

    Homepage: Projects, About, Contact

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,17 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import classnames from "classnames";
 import { TiledProject } from "../components/projects";
-import { Navigation, Container, Hero, Section } from "../layouts/";
-import styles from "../styles/pages/Home.module.css";
+import { StackedProject } from "../components/projects/Stacked";
+import { Navigation, Container, Hero, Section, Footer } from "../layouts/";
+import styles from "../styles/pages/Home.module.scss";
 import utilStyles from "../styles/utils.module.scss";
+import { Carousel } from "../components/carousel";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Beth Larcombe</title>
         <meta
@@ -21,71 +24,102 @@ const Home: NextPage = () => {
 
       <Navigation />
 
-      <main className={styles.main}>
-        <Container>
-          <div className={utilStyles.grid}>
-            <Hero
-              title="Beth Larcombe"
-              subTitle="Graphic Designer"
-              body="A multi-disciplined designer based in Southampton with over 6 years
-              experience on a variety of brand, print &amp; digital projects."
-            />
-            <TiledProject
-              images={[
-                {
-                  src: "https://www.portseattle.org/sites/default/files/styles/detailpageimagesize/public/2019-12/Police%20K9%20team_181114_307.jpg?itok=Ac3I1crY",
-                  alt: "Cute puppy",
-                },
-                {
+      <main>
+        <section className={styles.hero}>
+          <Container>
+            <div className={classnames(utilStyles.grid, styles.heroGrid)}>
+              <Hero
+                title="Beth Larcombe"
+                subTitle="Graphic Designer"
+                body="A multi-disciplined designer based in Southampton with over 6 years
+                experience on a variety of brand, print &amp; digital projects."
+              />
+              <TiledProject
+                images={[
+                  {
+                    src: "https://www.portseattle.org/sites/default/files/styles/detailpageimagesize/public/2019-12/Police%20K9%20team_181114_307.jpg?itok=Ac3I1crY",
+                    alt: "Cute puppy",
+                  },
+                  {
+                    src: "https://www.oliverpetcare.com/wp-content/uploads/2020/12/pexels-helena-lopes-1790446.jpg",
+                    alt: "Another one omg",
+                  },
+                ]}
+                button={{
+                  text: "Check It Out",
+                  href: "/todo!",
+                }}
+              />
+            </div>
+          </Container>
+        </section>
+        <Section background="gradient" title="Recent Work" id="projects">
+          <Carousel
+            items={[
+              <StackedProject
+                key={0}
+                image={{
                   src: "https://www.oliverpetcare.com/wp-content/uploads/2020/12/pexels-helena-lopes-1790446.jpg",
-                  alt: "Another one omg",
-                },
-              ]}
-              button={{
-                text: "Check It Out",
-                href: "/todo!",
-              }}
-            />
-          </div>
-        </Container>
-        <Section background="gradient" title="Recent Work">
-          Carousel
+                  alt: "cuuute puppy",
+                }}
+                category={"Brand & Digital"}
+                title={"Willo Remedies"}
+                button={{
+                  href: "/todo",
+                  text: "view project",
+                }}
+              />,
+              <StackedProject
+                key={1}
+                image={{
+                  src: "https://www.oliverpetcare.com/wp-content/uploads/2020/12/pexels-helena-lopes-1790446.jpg",
+                  alt: "cuuute puppy",
+                }}
+                category={"Brand & Digital"}
+                title={"Tinkr"}
+                button={{
+                  href: "/todo",
+                  text: "view project",
+                }}
+              />,
+              <StackedProject
+                key={2}
+                image={{
+                  src: "https://www.oliverpetcare.com/wp-content/uploads/2020/12/pexels-helena-lopes-1790446.jpg",
+                  alt: "cuuute puppy",
+                }}
+                category={"Brand & Digital"}
+                title={"The Blend Academy"}
+                button={{
+                  href: "/todo",
+                  text: "view project",
+                }}
+              />,
+            ]}
+          />
+        </Section>
+        <Section contain variant="about" id="about">
+          <h2 className={classnames(utilStyles.heading2, utilStyles.margin0)}>
+            Time flies
+          </h2>
+          <h3 className={utilStyles.heading3}>
+            Over 6 years of design adventures
+          </h3>
+          <p>
+            With experience in brand agencies, digital studios and in-house,
+            I’ve spent the last few years honing my craft. My absolutely
+            favourite projects to work on are brand creation, refreshes and
+            digital, but I have a whole heap of print experience to boot too.
+            Basically, whatever you need, I’ve got you covered.
+          </p>
+          <a className={utilStyles.button} href="https://linkedin.com">
+            Catch me on Linkedin
+          </a>
         </Section>
       </main>
 
-      <footer>
-        <Container>
-          <h2>Like what you see?</h2>
-          <p>
-            I’m available for projects big and small and would love to hear what
-            you’re planning. Feel free to give me a shout and we can have a
-            chat.
-          </p>
-
-          <form>
-            <label htmlFor="name">Your name</label>
-            <input id="name" type="text" placeholder="Your name" />
-
-            <label htmlFor="email">Your email addreess</label>
-            <input id="email" type="text" placeholder="Your email address" />
-
-            <label htmlFor="email">Your message</label>
-            <input id="email" type="text" placeholder="Your message" />
-
-            <button type="submit">Submit</button>
-          </form>
-
-          <p>
-            Build by{" "}
-            <a href="https://mattie.world" target="_blank" rel="noreferrer">
-              mattie.world
-            </a>
-          </p>
-          <span>bee</span>
-          <a href="#">Back to the top &uarr;</a>
-        </Container>
-      </footer>
-    </div>
+      <Footer />
+    </>
   );
 };
 

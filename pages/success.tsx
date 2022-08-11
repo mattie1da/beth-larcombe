723445e576874f77f1e9b1305356eb74c93384dc
Matt Wyatt <hello@mattie.world>
2022-08-11 21:12:25 +0100

    Wrap success page in a container

--- pages/success.tsx
+++ pages/success.tsx
@@ -1,19 +1,22 @@
 import type { NextPage } from "next";
 import Link from "next/link";
+import { Container } from "../layouts";
 import styles from "../styles/pages/Success.module.scss";
 import utilStyles from "../styles/utils.module.scss";
 
 const Success: NextPage = () => {
   return (
-    <div className={styles.success}>
-      <div className={styles.content}>
-        <h1 className={utilStyles.heading1}>Thank you!</h1>
-        <h2 className={utilStyles.heading2}>I&apos;ll be in touch - Beth</h2>
-        <Link href="/">
-          <a className={utilStyles.button}>&larr; Back to home</a>
-        </Link>
+    <Container>
+      <div className={styles.success}>
+        <div className={styles.content}>
+          <h1 className={utilStyles.heading1}>Thank you!</h1>
+          <h2 className={utilStyles.heading2}>I&apos;ll be in touch - Beth</h2>
+          <Link href="/">
+            <a className={utilStyles.button}>&larr; Back to home</a>
+          </Link>
+        </div>
       </div>
-    </div>
+    </Container>
   );
 };
 

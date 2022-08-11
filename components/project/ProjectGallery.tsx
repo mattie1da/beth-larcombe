ad284c9b279944d522676f8a3703b3c6bdd2ee5d
Matt Wyatt <hello@mattie.world>
2022-08-11 19:15:33 +0100

    Project Gallery: Nicer loop through data

--- components/project/ProjectGallery.tsx
+++ components/project/ProjectGallery.tsx
@@ -1,65 +1,20 @@
 import Image from "next/image";
 import styles from "../../styles/components/project/ProjectGallery.module.scss";
+import { ImageInterface } from "../../types";
 
 export const ProjectGallery = ({ images }) => {
   return (
     <div className={styles.grid}>
-      <div>
-        <Image
-          src={images[0].src}
-          alt={images[0].alt}
-          layout="fill"
-          objectFit="cover"
-        />
-      </div>
-      <div>
-        <Image
-          src={images[0].src}
-          alt={images[0].alt}
-          layout="fill"
-          objectFit="cover"
-        />
-      </div>
-      <div>
-        <Image
-          src={images[0].src}
-          alt={images[0].alt}
-          layout="fill"
-          objectFit="cover"
-        />
-      </div>
-      <div>
-        <Image
-          src={images[0].src}
-          alt={images[0].alt}
-          layout="fill"
-          objectFit="cover"
-        />
-      </div>
-      <div>
-        <Image
-          src={images[0].src}
-          alt={images[0].alt}
-          layout="fill"
-          objectFit="cover"
-        />
-      </div>
-      <div>
-        <Image
-          src={images[0].src}
-          alt={images[0].alt}
-          layout="fill"
-          objectFit="cover"
-        />
-      </div>
-      <div>
-        <Image
-          src={images[0].src}
-          alt={images[0].alt}
-          layout="fill"
-          objectFit="cover"
-        />
-      </div>
+      {images.map((image: ImageInterface, index: number) => (
+        <div key={index}>
+          <Image
+            src={image.src}
+            alt={image.alt}
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+      ))}
     </div>
   );
 };

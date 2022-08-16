045a66ce489da39db288baeb7abb74682824060b
Matt Wyatt <hello@mattie.world>
2022-08-16 21:10:05 +0100

    Project Intro renders json html

--- components/project/ProjectIntro.tsx
+++ components/project/ProjectIntro.tsx
@@ -4,14 +4,24 @@ import { ProjectIntroInterface } from "../../types";
 import styles from "../../styles/components/project/ProjectIntro.module.scss";
 import { List } from "../List";
 
-export const ProjectIntro = ({ description, list }: ProjectIntroInterface) => {
+export const ProjectIntro = ({
+  description,
+  list,
+  bulletColor,
+}: ProjectIntroInterface) => {
   return (
     <Container>
       <div className={styles.wrapper}>
-        <div className={styles.description}>{description}</div>
+        <div
+          className={styles.description}
+          dangerouslySetInnerHTML={{ __html: description }}
+        ></div>
         <div>
-          <p>This project included:</p>
-          <List list={list} />
+          <List
+            title="This project included:"
+            list={list}
+            bulletColor={bulletColor}
+          />
         </div>
       </div>
     </Container>

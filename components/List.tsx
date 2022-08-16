9051785cdb3ac654363cc0dada6b51282e3c2d84
Matt Wyatt <hello@mattie.world>
2022-08-16 21:09:16 +0100

    Pass bullet color to list as a css variable

--- components/List.tsx
+++ components/List.tsx
@@ -1,19 +1,29 @@
 import styles from "../styles/components/List.module.scss";
+import { CSSProperties } from "react";
 
 interface ListInterface {
   list: string[];
+  title: string;
+  bulletColor: string;
 }
 
 export const List = (props: ListInterface) => {
-  const { list } = props;
+  const { list, title, bulletColor } = props;
 
   return (
-    <ul className={styles.wrapper}>
-      {list.map((item: string) => (
-        <li className={styles.item} key={item}>
-          {item}
-        </li>
-      ))}
-    </ul>
+    <>
+      <p className={styles.title}>{title}</p>
+      <ul className={styles.wrapper}>
+        {list.map((item: string) => (
+          <li
+            className={styles.item}
+            key={item}
+            style={{ "--bullet-color": bulletColor } as CSSProperties}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };

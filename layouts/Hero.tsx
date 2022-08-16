f0d035e9cb5bc4ce785ca6f107ad3bc18087cd5b
Matt Wyatt <hello@mattie.world>
2022-08-16 21:13:21 +0100

    Hero: Handle both heros in one component

--- layouts/Hero.tsx
+++ layouts/Hero.tsx
@@ -1,18 +1,85 @@
-import styles from "../styles/layouts/Hero.module.scss";
+import classnames from "classnames";
+import Image from "next/image";
 import utilStyles from "../styles/utils.module.scss";
+import styles from "../styles/layouts/Hero.module.scss";
+import { HeroInterface } from "../types";
+import { gsap } from "gsap";
+import { useEffect, useRef } from "react";
 
-interface HeroInterface {
-  title: string;
-  subTitle: string;
-  body: string;
-}
+export const Hero = ({
+  title,
+  subTitle,
+  body,
+  image,
+  colors,
+}: HeroInterface) => {
+  const figureRef = useRef(null);
+  const titleRef = useRef(null);
+  const subTitleRef = useRef(null);
+  const overlayRef = useRef(null);
 
-export const Hero = ({ title, subTitle, body }: HeroInterface) => {
-  return (
-    <div className={styles.wrapper}>
-      <h1 className={utilStyles.heading1}>{title}</h1>
-      <h2 className={utilStyles.heading2}>{subTitle}</h2>
-      <p className={styles.body}>{body}</p>
+  useEffect(() => {
+    if (image) {
+      gsap.to(titleRef.current, {
+        scrollTrigger: {
+          trigger: figureRef.current,
+          scrub: true,
+          start: "0%",
+          end: "30%",
+        },
+        color: colors.primary,
+      });
+      gsap.to(subTitleRef.current, {
+        scrollTrigger: {
+          trigger: figureRef.current,
+          scrub: true,
+          start: "5%",
+          end: "30%",
+        },
+        color: colors.secondary,
+      });
+      gsap.to(overlayRef.current, {
+        background: `linear-gradient(transparent, #fffdea)`,
+        scrollTrigger: {
+          trigger: figureRef.current,
+          scrub: true,
+          start: "0%",
+          end: "20%",
+        },
+      });
+    }
+  }, [image]);
+  return image ? (
+    <figure
+      ref={figureRef}
+      className={classnames(styles.hero, {
+        [styles.project]: image,
+      })}
+    >
+      <Image src={image.src} alt={image.alt} layout="fill" objectFit="cover" />
+      <figcaption className={styles.content}>
+        <h1
+          ref={titleRef}
+          className={classnames(utilStyles.heading1, styles.title)}
+        >
+          {title}
+        </h1>
+        <h2
+          ref={subTitleRef}
+          className={classnames(utilStyles.heading2, styles.category)}
+        >
+          {subTitle}
+        </h2>
+      </figcaption>
+      <aside ref={overlayRef} className={styles.overlay}></aside>
+    </figure>
+  ) : (
+    <div className={styles.hero}>
+      <div>
+        <h1 className={utilStyles.heading1}>{title}</h1>
+        <h2 className={utilStyles.heading2}>{subTitle}</h2>
+        <p className={styles.body}>{body}</p>
+      </div>
     </div>
   );
 };

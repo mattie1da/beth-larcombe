f1f31f000b41eadcd52cbb99b3b45b548acc9f13
Matt Wyatt <hello@mattie.world>
2022-08-16 21:14:15 +0100

    register scrolltrigger plugin globally

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }

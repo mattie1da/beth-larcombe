9fdc16ee942f33d83c94d9f771cfd02a7c17b753
Matt Wyatt <hello@mattie.world>
2022-09-01 11:53:29 +0100

    Add colors type to hero interface

--- types/hero.ts
+++ types/hero.ts
@@ -1,6 +1,10 @@
 import { ImageInterface } from "./";
 
 export interface HeroInterface {
+  colors: {
+    primary: string;
+    secondary: string;
+  };
   title: string;
   subTitle: string;
   body: string;

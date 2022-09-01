538abe4a3577557f4b6afa5d434da314af75de41
Matt Wyatt <hello@mattie.world>
2022-09-01 18:07:43 +0100

    WIP: Project gallery animatioins

--- components/project/ProjectGallery.tsx
+++ components/project/ProjectGallery.tsx
@@ -4,18 +4,50 @@ import styles from "../../styles/components/project/ProjectGallery.module.scss";
 import { ImageInterface } from "../../types";
 import { IconFlower } from "../icons";
 
+import { gsap } from "gsap";
+import { useEffect, useRef } from "react";
+
 interface ProjectGalleryInterface {
   images: ImageInterface[];
 }
 
 export const ProjectGallery = (props: ProjectGalleryInterface) => {
   const { images } = props;
+  const galleryItems = useRef([]);
+  const gridWrapper = useRef(null);
+
+  useEffect(() => {
+    gsap.to(galleryItems.current[1], {
+      scrollTrigger: {
+        trigger: gridWrapper.current,
+        scrub: true,
+        start: "0%",
+        end: "100%",
+      },
+      y: 250,
+    });
+
+    gsap.to(galleryItems.current[2], {
+      scrollTrigger: {
+        trigger: gridWrapper.current,
+        scrub: true,
+        start: "0%",
+        end: "100%",
+      },
+      y: -120,
+    });
+
+    // TODO: Last 3 items?
+  }, []);
 
   return (
     <>
-      <div className={classnames(styles.grid, styles.gridTwo)}>
+      <div
+        ref={gridWrapper}
+        className={classnames(styles.grid, styles.gridTwo)}
+      >
         {images.slice(0, 4).map((image: ImageInterface, index: number) => (
-          <div key={index}>
+          <div ref={(el) => (galleryItems.current[index] = el)} key={index}>
             {index === 1 ? (
               <>
                 <Image
